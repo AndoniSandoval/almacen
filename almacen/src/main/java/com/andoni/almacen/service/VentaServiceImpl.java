ead2dbf8b10faee917a020636af70ef4da368008
AndoniSandoval <andoni2020lopez@gmail.com>
2026-06-04 09:22:21 -0600

    fix: VentaServiceImpl

--- almacen/src/main/java/com/andoni/almacen/service/VentaServiceImpl.java
+++ almacen/src/main/java/com/andoni/almacen/service/VentaServiceImpl.java
@@ -1,11 +1,15 @@
 package com.andoni.almacen.service;
 
+import com.andoni.almacen.dto.ventas.VentaRequest;
+import com.andoni.almacen.dto.ventas.VentaResponse;
 import com.andoni.almacen.repositories.VentaRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -14,4 +18,24 @@ import org.springframework.stereotype.Service;
 public class VentaServiceImpl implements VentaService{
 
     private final VentaRepository ventaRepository;
+
+    @Override
+    public List<VentaResponse> listar() {
+        throw new UnsupportedOperationException("Pendiente implementar");
+    }
+
+    @Override
+    public VentaResponse obtenerPorId(Long id) {
+        throw new UnsupportedOperationException("Pendiente implementar");
+    }
+
+    @Override
+    public VentaResponse registrar(VentaRequest request) {
+        throw new UnsupportedOperationException("Pendiente implementar");
+    }
+
+    @Override
+    public void cancelar(Long id) {
+        throw new UnsupportedOperationException("Pendiente implementar");
+    }
 }
