4554bfdb00716fb385f2917d8a86476aebe780e5
AndoniSandoval <andoni2020lopez@gmail.com>
2026-06-03 18:24:25 -0600

    fix: VentaMapper primera version

--- almacen/src/main/java/com/andoni/almacen/mappers/VentaMapper.java
+++ almacen/src/main/java/com/andoni/almacen/mappers/VentaMapper.java
@@ -7,6 +7,9 @@ import com.andoni.almacen.entities.Venta;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Component
 @AllArgsConstructor
 public class VentaMapper {
@@ -14,10 +17,39 @@ public class VentaMapper {
     private final SucursalMapper sucursalMapper;
 
     public VentaResponse entidadAResponse (Venta venta){
-        return null;
+        if (venta == null) return null;
+
+        List<DetalleVentaResponse> detalles =
+                venta.getDetalleVenta()
+                        .stream()
+                        .map(this::detalleAResponse)
+                        .toList();
+
+        BigDecimal total =
+                venta.getDetalleVenta()
+                        .stream()
+                        .map(DetalleVenta::calcularSubtotal)
+                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return new VentaResponse(
+                venta.getId(),
+                venta.getFecha().toString(),
+                venta.getEstadoVenta().getDescripcion(),
+                sucursalMapper.entidadAResponse(venta.getSucursal()),
+                detalles,
+                total
+        );
     }
 
     public DetalleVentaResponse detalleAResponse (DetalleVenta detalle) {
-        return null;
+        if (detalle == null) return null;
+
+        return new DetalleVentaResponse(
+                detalle.getProducto().getId(),
+                detalle.getProducto().getNombre(),
+                detalle.getCantidadProducto(),
+                detalle.getPrecioProducto(),
+                detalle.calcularSubtotal()
+        );
     }
 }
