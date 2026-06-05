25f419044a5c875bbaac4e1dae128a52241ada6c
AndoniSandoval <andoni2020lopez@gmail.com>
2026-06-04 23:36:49 -0600

    feat: Registrar, Eliminar y ListarCanceladas en ventas

--- almacen/src/main/java/com/andoni/almacen/service/VentaServiceImpl.java
+++ almacen/src/main/java/com/andoni/almacen/service/VentaServiceImpl.java
@@ -1,18 +1,24 @@
 package com.andoni.almacen.service;
 
+import com.andoni.almacen.dto.ventas.DetalleVentaRequest;
 import com.andoni.almacen.dto.ventas.VentaRequest;
 import com.andoni.almacen.dto.ventas.VentaResponse;
+import com.andoni.almacen.entities.DetalleVenta;
+import com.andoni.almacen.entities.Producto;
+import com.andoni.almacen.entities.Sucursal;
 import com.andoni.almacen.entities.Venta;
 import com.andoni.almacen.enums.EstadoVenta;
 import com.andoni.almacen.exceptions.RecursoNoEncontradoException;
 import com.andoni.almacen.mappers.VentaMapper;
+import com.andoni.almacen.repositories.ProductoRepository;
+import com.andoni.almacen.repositories.SucursalRepository;
 import com.andoni.almacen.repositories.VentaRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -25,12 +31,25 @@ public class VentaServiceImpl implements VentaService{
 
     private final VentaMapper ventaMapper;
 
+    private final ProductoRepository productoRepository;
+
+    private final SucursalRepository sucursalRepository;
+
     @Override
     public List<VentaResponse> listar() {
         log.info("Listando ventas registradas");
 
         return ventaRepository
                 .findByEstadoVenta(EstadoVenta.REGISTRADA)
+                .stream()
+                .map(ventaMapper::entidadAResponse)
+                .toList();
+    }
+
+    public List<VentaResponse> listarCanceladas() {
+        log.info("Listando ventas canceladas...");
+
+        return ventaRepository.findByEstadoVenta(EstadoVenta.CANCELADA)
                 .stream()
                 .map(ventaMapper::entidadAResponse)
                 .toList();
@@ -44,7 +63,7 @@ public class VentaServiceImpl implements VentaService{
                 id,
                 EstadoVenta.REGISTRADA
         ).orElseThrow(() ->
-                new RecursoNoEncontradoException("Recurso no encontrado con id: " + id)
+                new RecursoNoEncontradoException("Venta no encontrado con id: " + id)
         );
 
         return ventaMapper.entidadAResponse(venta);
@@ -52,11 +71,67 @@ public class VentaServiceImpl implements VentaService{
 
     @Override
     public VentaResponse registrar(VentaRequest request) {
-        throw new UnsupportedOperationException("Pendiente implementar REGISTRO");
+        log.info("Registrando nueva venta...");
+
+        Sucursal sucursal = sucursalRepository
+                .findById(request.idSucursal())
+                .orElseThrow(() ->
+                        new RecursoNoEncontradoException("Sucursal no encontrada con id:" + request.idSucursal()));
+
+        Venta venta = Venta.builder()
+                .estadoVenta(EstadoVenta.REGISTRADA)
+                .fecha(LocalDate.now())
+                .sucursal(sucursal)
+                .build();
+
+        for (DetalleVentaRequest detalleRequest : request.productos()) {
+
+            Producto producto = productoRepository.findById(
+                            detalleRequest.idProducto())
+                    .orElseThrow(() ->
+                            new RecursoNoEncontradoException(
+                                    "Producto no encontrado con id: "
+                                            + detalleRequest.idProducto()));
+
+            producto.descontarCantidad(detalleRequest.cantidadProducto());
+
+            DetalleVenta detalleVenta = DetalleVenta.builder()
+                    .producto(producto)
+                    .cantidadProducto(detalleRequest.cantidadProducto())
+                    .precioProducto(producto.getPrecio())
+                    .build();
+
+            venta.agregarDetalle(detalleVenta);
+        }
+
+        Venta ventaGuardada = ventaRepository.save(venta);
+
+        log.info("Venta registrada correctamente con id: {}",
+                ventaGuardada.getId());
+
+        return ventaMapper.entidadAResponse(ventaGuardada);
     }
 
     @Override
     public void cancelar(Long id) {
-        throw new UnsupportedOperationException("Pendiente implementar");
+        log.info("Cancelando venta con id: {}" + id);
+
+        Venta venta = ventaRepository.findByIdAndEstadoVenta(
+                id, EstadoVenta.REGISTRADA)
+                .orElseThrow(() ->
+                        new RecursoNoEncontradoException("Venta no encontrada con id: " + id));
+
+        for (DetalleVenta detalle : venta.getDetalleVenta()) {
+
+            Producto producto = detalle.getProducto();
+
+            producto.aumentarCantidad(detalle.getCantidadProducto());
+        }
+
+        venta.cancelada();
+
+        ventaRepository.save(venta);
+
+        log.info("Venta cancelada correctamente");
     }
 }
